445bb26117317a399db8048fb404bd3fe6ec4c0f
Alfieri Unciano <45444514+alfieriunciano@users.noreply.github.com>
2019-05-12 22:21:50 -0700

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE `burgers_db`;
+
+
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('4X4', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Animal Style', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('BLT', false, CURRENT_TIMESTAMP);
